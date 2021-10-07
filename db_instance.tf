8e24ee7c72f46658256ba7111c7545e17244b4f1
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-10-07 12:56:41 +0900

    Fix warning

--- db_instance.tf
+++ db_instance.tf
@@ -1,8 +1,9 @@
 resource "aws_db_instance" "main" {
-  instance_class        = "db.t2.micro"
-  copy_tags_to_snapshot = true
-  skip_final_snapshot   = true
-  max_allocated_storage = 1000
+  instance_class          = "db.t2.micro"
+  copy_tags_to_snapshot   = true
+  skip_final_snapshot     = true
+  max_allocated_storage   = 1000
+  backup_retention_period = 7
   tags = {
     "Name" = "christchurches-map-ecs-rds"
   }
