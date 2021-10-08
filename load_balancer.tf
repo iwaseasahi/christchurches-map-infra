6a95c79481e10282441411d694d69e6fbaa10ad3
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-10-08 21:37:58 +0900

    Delete internal

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,6 +1,5 @@
 resource "aws_lb" "main" {
   name                       = "christchurches-map-ecs-alb"
   load_balancer_type         = "application"
-  internal                   = false
   drop_invalid_header_fields = true
 }
