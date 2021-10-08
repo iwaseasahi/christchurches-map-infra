b58bdde0ed0df60b9d9904ad1b7db2b6c32998ec
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-10-08 21:35:43 +0900

    Fix tfsec
    https://github.com/iwaseasahi/christchurches-map-infra/pull/28/checks?check_run_id=3838493354

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,4 +1,6 @@
 resource "aws_lb" "main" {
-  name               = "christchurches-map-ecs-alb"
-  load_balancer_type = "application"
+  name                       = "christchurches-map-ecs-alb"
+  load_balancer_type         = "application"
+  internal                   = false
+  drop_invalid_header_fields = true
 }
