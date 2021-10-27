2b92d98c70194970f9026edda62a1e738b3be65e
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-10-27 15:47:49 +0900

    Add ecs_service.tf

---
+++ ecs_service.tf
@@ -0,0 +1,38 @@
+resource "aws_ecs_service" "main" {
+  name            = "cc-map-service-bg-deploy"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  iam_role        = "aws-service-role"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "christchurches-map-nginx"
+    container_port   = 80
+  }
+
+  enable_ecs_managed_tags           = true
+  health_check_grace_period_seconds = 0
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0"
+  tags                              = {}
+  tags_all                          = {}
+  wait_for_steady_state             = false
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups = [
+      aws_security_group.christchurches-map-ecs.id,
+    ]
+    subnets = [
+      aws_subnet.public_subnet1.id,
+      aws_subnet.public_subnet2.id,
+    ]
+  }
+
+  timeouts {}
+}
